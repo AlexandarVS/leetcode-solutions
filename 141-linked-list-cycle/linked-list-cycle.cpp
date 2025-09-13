d009c14a54a74265b854b5a174a99b1f74c72ca4
Aleksandar Slavov <117994598+AlexandarVS@users.noreply.github.com>
2025-09-14 01:30:39 +0300

    Time: 7 ms (81.92%) | Memory: 11.9 MB (53.25%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,7 +9,7 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head == nullptr){
+        if(head == nullptr || head->next == nullptr){
             return false;
         }
 
