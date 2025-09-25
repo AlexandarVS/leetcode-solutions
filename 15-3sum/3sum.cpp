464c97a1dddc282f40ac0726503307378aea2cca
Aleksandar Slavov <117994598+AlexandarVS@users.noreply.github.com>
2025-09-25 15:01:44 +0300

    Time: 47 ms (70.35%) | Memory: 28.9 MB (99.02%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& nums, int target, int left, int right){
-        while(left <= right){
-            int middle = left + (right - left) / 2;
-            if(nums[middle] == target){
-                return middle;
-            }else if(nums[middle] < target){
-                left = middle + 1;
-            }else{
-                right = middle - 1;
-            }
-        }
-        return INT_MAX;
-    }
-
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         vector<vector<int>> result;
-        for(int i = 0;i<nums.size();i++){
+        for(int i = 0;i<nums.size() - 2;i++){
             if (i > 0 && nums[i] == nums[i - 1]) continue;
-            for(int j = i + 1;j<nums.size();j++){
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-                int target = -(nums[i] + nums[j]);
-                int k = binarySearch(nums,target, j + 1, nums.size() - 1);
-                if(k != INT_MAX){
-                    result.push_back({nums[i], nums[j], nums[k]});
+            
+            int left = i + 1;
+            int right = nums.size() - 1;
+
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if(sum == 0){
+                    result.push_back({nums[i], nums[left], nums[right]});
+
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                }else if(sum < 0){
+                    left++;
+                }else{
+                    right--;
                 }
             }
         }
