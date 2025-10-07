71923bc20c763dba6a7b3e80b6a7b8ce7b0c97b6
Aleksandar Slavov <117994598+AlexandarVS@users.noreply.github.com>
2025-10-07 15:50:38 +0300

    Time: 1 ms (28.70%) | Memory: 42.7 MB (53.20%) - LeetSync

---
+++ 2020-remove-one-element-to-make-the-array-strictly-increasing/remove-one-element-to-make-the-array-strictly-increasing.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canBeIncreasing(int[] nums) {
+        int countRemovableElements = 0;
+        int iterator = 1;
+        while(iterator != nums.length){
+            if(nums[iterator - 1] >= nums[iterator]){
+                countRemovableElements++;
+                if (iterator > 1 && nums[iterator] <= nums[iterator - 2]) {
+                    nums[iterator] = nums[iterator - 1];
+                }
+            }
+            iterator++;
+        }
+        return countRemovableElements <= 1;
+    }
+}
