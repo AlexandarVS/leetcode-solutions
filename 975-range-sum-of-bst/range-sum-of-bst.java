2b6a37b8dd4dbe53b7d74b0e7b348ae40a71a34e
Aleksandar Slavov <117994598+AlexandarVS@users.noreply.github.com>
2025-10-09 02:14:36 +0300

    Time: 0 ms (100.00%) | Memory: 51.5 MB (36.48%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.java
+++ 975-range-sum-of-bst/range-sum-of-bst.java
@@ -19,9 +19,16 @@ class Solution {
             return 0;
         }
 
+        if(root.val < low){
+            return rangeSumBST(root.right,low,high);
+        }else if(root.val > high){
+            return rangeSumBST(root.left,low,high);
+        }
+
         if(root.val >= low && root.val <= high){
             return root.val + rangeSumBST(root.left,low,high) + rangeSumBST(root.right,low,high);
         }
+
         return rangeSumBST(root.left,low,high) + rangeSumBST(root.right,low,high);
     }
 }
